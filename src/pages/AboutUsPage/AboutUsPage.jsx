434f1f8fdad71a951b3c26b1064e3c7f8375fd0f
David <david48kariuki@gmail.com>
2025-07-02 12:05:24 +0300

    feat: Create AboutUsPage shell

---
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -0,0 +1,17 @@
+// src/pages/AboutUsPage/AboutUsPage.jsx
+import React from "react";
+import styles from "./AboutUsPage.module.css";
+
+function AboutUsPage() {
+  return (
+    <div className={styles.aboutUsPage}>
+      <h1>About Greenwood International School</h1>
+      <p>
+        This page will detail our mission, vision, background, and leadership
+        team.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUsPage;
