08000592ea5e6d06e7b04f59cf366bc64557d89d
David <david48kariuki@gmail.com>
2025-07-02 12:06:59 +0300

    feat: Create AcademicsPage shell

---
+++ src/pages/AcademicsPage/AcademicsPage.jsx
@@ -0,0 +1,17 @@
+// src/pages/AcademicsPage/AcademicsPage.jsx
+import React from "react";
+import styles from "./AcademicsPage.module.css";
+
+function AcademicsPage() {
+  return (
+    <div className={styles.academicsPage}>
+      <h1>Academics at Greenwood</h1>
+      <p>
+        This page will cover departments, curriculum, calendar, and exam
+        performance.
+      </p>
+    </div>
+  );
+}
+
+export default AcademicsPage;
