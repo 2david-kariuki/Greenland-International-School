78ece454b9eeb2b06472658e05e9824640bfa303
David <david48kariuki@gmail.com>
2025-07-02 12:08:30 +0300

    feat: Create ActivitiesPage shell

---
+++ src/pages/ActivitiesPage/ActivitiesPage.jsx
@@ -0,0 +1,17 @@
+// src/pages/ActivitiesPage/ActivitiesPage.jsx
+import React from "react";
+import styles from "./ActivitiesPage.module.css";
+
+function ActivitiesPage() {
+  return (
+    <div className={styles.activitiesPage}>
+      <h1>Extracurricular Activities & Tours</h1>
+      <p>
+        This page will showcase sports, clubs, community service, competitions,
+        and educational tours.
+      </p>
+    </div>
+  );
+}
+
+export default ActivitiesPage;
